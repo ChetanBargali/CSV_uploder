d2ffb2b7c231c00bff3c80898af2dc6974ed6b0d
Chetan Bargali <chetanbargali022@gmail.com>
2023-10-15 20:24:27 +0530

    mongoDb connected

---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+require('dotenv').config()
+//connect database
+mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+
+//acquire the connection(to check if it is sucessful)
+const db=mongoose.connection;
+
+
+//error
+db.on('error',console.error.bind(console,"Error connecting to mongoDB"));
+
+
+//if succesfull and running the print the massege 
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports=db;
